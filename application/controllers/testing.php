8214662ac777e3b8f3d9c0a5d1bcdb634f1a6108
Hendrik Jan van Meerveld <H.J.vanmeerveld@uu.nl>
2013-06-27 16:17:59 +0200

    added some unit test for QUnit/Sinon, started implementing the backbone structure

--- application/controllers/testing.php
+++ application/controllers/testing.php
@@ -3,12 +3,56 @@
 class Testing extends CI_Controller {
 
 	/**
-	 * QUnit test page for Javascript testing on the client
-	 */
+	* QUnit test page for Javascript testing on the client
+	*/
 	public function qunit()
 	{
 		$this->load->helper('url');
 		$this->load->view('testing/qunit');
+	}
+
+	/**
+	* testobjects -- Mocks the API
+	*/
+	public function mocks($one=null)
+	{
+		// for: model.fetch
+		// GET /mocks/33
+		if ($this->server('REQUIST_METHOD') === 'GET' AND $one==33)
+		{
+			// return object with id 33 and status 200: success
+		}
+		// for: model.save
+		// PUT /mocks/33
+		elseif ($this->server('REQUIST_METHOD') === 'PUT' AND $one==33)
+		{
+			// return status code 200: success, but no content to return
+		}
+		// for: model.destroy
+		// DELETE /mocks/33
+		elseif ($this->server('REQUIST_METHOD') === 'DELETE' AND $one==33)
+		{
+			// return status code 204: success, but no content to return
+		}
+		// for: collection.add
+		// POST /mocks
+		elseif ($this->server('REQUIST_METHOD') === 'POST' AND is_null($one))
+		{
+			// return same object, but including the new id for the object
+			// return status code 201: resource created
+		}
+		// for: collection.fetch
+		// GET /mocks
+		elseif ($this->server('REQUIST_METHOD') === 'GET' AND is_null($one))
+		{
+			// return collection
+			// return status code 200: success
+		}
+		else
+		{
+			// return 404: not found
+		}
+
 	}
 }
 
--- application/views/testing/qunit.php
+++ application/views/testing/qunit.php
@@ -2,12 +2,21 @@
 <html>
 <head>
     <title>QUnit Test Suite</title>
-     <link rel="stylesheet" href="<?php echo base_url('css/qunit.css'); ?>">
-     <script src="<?php echo base_url('scripts/qunit.js'); ?>"></script>
-     <!-- Your application -->
-     <script src="<?php echo base_url('scripts/app.js'); ?>"></script>
-     <!-- Your tests -->
-     <script src="<?php echo base_url('scripts/tests.js'); ?>"></script>
+    <link rel="stylesheet" href="<?php echo base_url('css/qunit-1.12.0.css'); ?>">
+
+    <!-- For testing -->
+    <script src="<?php echo base_url('scripts/qunit-1.12.0.js'); ?>"></script>
+    <script src="<?php echo base_url('scripts/sinon-1.7.3.js'); ?>"></script>
+    <script src="<?php echo base_url('scripts/sinon-qunit-1.0.0.js'); ?>"></script>
+    <!-- Dependencies for ultraform.js -->
+    <script src="<?php echo base_url('scripts/jquery-1.8.3.min.js'); ?>"></script>
+    <script src="<?php echo base_url('scripts/jquery.validVal-4.4.1-min.js'); ?>"></script>
+    <script src="<?php echo base_url('scripts/underscore.js'); ?>"></script>
+    <script src="<?php echo base_url('scripts/backbone.js'); ?>"></script>
+    <!-- Your application -->
+    <script src="<?php echo base_url('scripts/ultraform.js'); ?>"></script>
+    <!-- Your tests -->
+    <script src="<?php echo base_url('scripts/tests.js'); ?>"></script>
 </head>
 <body>
     <h1 id="qunit-header">QUnit Test Suite</h1>
