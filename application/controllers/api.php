526102bbb8c204c9a7492bca9b627281b9d0578f
Simon Kort <simon.kort@gmail.com>
2013-07-22 15:18:19 +0200

    Reworked loading. You can now use $this->ultraform->load('name_form'); to load a json file in the /forms directory of application.

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -12,21 +12,12 @@ class Api extends CI_Controller {
 		// Load ultraform
 		$this->load->library('ultraform');
 		
-		// Get the contact and signup forms
-		$form_contact = get_form('contact');
-		$form_signup = get_form('signup');
-		
 		echo 'This is the API speaking<br><br>';
 		
-		echo '<pre>';
-		print_r($form_contact->get_elements());
-		print_r($form_signup->get_elements());
-		echo '</pre>';
+		echo 'Loading form<br>';
+		$form = $this->ultraform->load('contact');
 		
-		echo 'And now for some JSON!<br><br>';
-		
-		echo json_encode($form_contact->get_elements());
-		echo json_encode($form_signup->get_elements());
+		echo $this->ultraform;
 	}
 }
 
