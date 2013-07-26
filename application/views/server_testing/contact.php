a20e054e864d6202dde9d279c43ea6b494cd6676
Hendrik Jan van Meerveld <H.J.vanmeerveld@uu.nl>
2013-07-26 14:24:09 +0200

    for testing server/client

--- application/views/server_testing/contact.php
+++ application/views/server_testing/contact.php
@@ -5,7 +5,7 @@
 	</head>
 	<body>
 		<h1>Contact form</h1>
-		
+
 		<p>If you would like to contact us, please fill out the following form.</p>
 
 		<div id="contact_form">
@@ -18,5 +18,20 @@
 			<?php echo $contact_form->render('submit');?>
 			<?php echo $contact_form->render('close');?>
 		</div>
+
+        <script>var $base_url = "<?php echo base_url(); ?>"</script>
+        <!-- For testing -->
+        <script src="<?php echo base_url('scripts/jquery-1.8.3.min.js'); ?>"></script>
+        <script src="<?php echo base_url('scripts/underscore.js'); ?>"></script>
+        <script src="<?php echo base_url('scripts/backbone.js'); ?>"></script>
+        <!-- Your application -->
+        <script src="<?php echo base_url('scripts/ultraform.js'); ?>"></script>
+        <script>
+
+            // initialize
+            Ultraform.initialize( $base_url+'server_testing' );
+
+        </script>
+
 	</body>
 </html>
