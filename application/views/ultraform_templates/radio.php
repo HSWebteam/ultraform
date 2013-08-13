3b9773ea042113637304907d96aeb2636447fd39
Simon Kort <simon.kort@gmail.com>
2013-08-13 13:11:33 +0200

    Radio button IDs now use the value.

--- application/views/ultraform_templates/radio.php
+++ application/views/ultraform_templates/radio.php
@@ -1,7 +1,7 @@
 <div>
 	<label for="<?php echo $name;?>"><?php echo $label?>: </label>
 	<?php foreach($options as $key => $value):?>
-	<input id="<?php echo $id;?>" type="radio" name="<?php echo $name;?>" value="<?php echo $key;?>"><?php echo $value;?> 
+	<input id="ufo-forms-<?php echo $name;?>-<?php echo $key;?>" type="radio" name="<?php echo $name;?>" value="<?php echo $key;?>"><?php echo $value;?> 
 	<?php endforeach;?>
 	<div id="<?php echo $id;?>_error" class="error"><?php echo $error_text;?></div>
 </div>
