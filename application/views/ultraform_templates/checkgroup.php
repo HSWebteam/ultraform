d843b0dd0c2b9a6d25029176baae5cd93f774138
Simon Kort <simon.kort@gmail.com>
2013-11-18 16:28:28 +0100

    Checkgroup template now uses the $key as the key to the option. This fixes the problem of $var[0] instead of $var['name'] in the POST variables.

--- application/views/ultraform_templates/checkgroup.php
+++ application/views/ultraform_templates/checkgroup.php
@@ -2,7 +2,7 @@
 	<label for="<?php echo $name;?>"><?php echo $label?>: </label>
 	<?php foreach($options as $key => $option):?>
 		<label for="ufo-<?php echo $formname;?>-<?php echo $name;?>-<?php echo $key;?>" class="checkbox">
-			<input id="ufo-<?php echo $formname;?>-<?php echo $name;?>-<?php echo $key;?>" type="checkbox" name="<?php echo $name;?>[]" value="<?php echo $option; ?>"<?php if(in_array($option, $selected)):?> checked<?php endif;?>>
+			<input id="ufo-<?php echo $formname;?>-<?php echo $name;?>-<?php echo $key;?>" type="checkbox" name="<?php echo $name;?>[<?php echo $key;?>]" value="<?php echo $option; ?>"<?php if(in_array($option, $selected)):?> checked<?php endif;?>>
 			<?php echo $option;?> 
 		</label>
 	<?php endforeach; ?>
