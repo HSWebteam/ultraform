60bbe857e583dc08ddb0c48b07c41384c84a5cde
Simon Kort <simon.kort@gmail.com>
2014-08-08 13:52:24 +0200

    Fixes #128

--- application/views/ultraform_templates/dropdown.php
+++ application/views/ultraform_templates/dropdown.php
@@ -1,7 +1,10 @@
 <div class="form-group">
 	<label for="<?php echo $e->name;?>" class="col-lg-3 control-label"><?php echo $e->label?><?php echo $e->required;?>: </label>
 	<div class="col-lg-9">
-		<select id="<?php echo $id;?>" class="form-control" name="<?php echo $name;?>">
+		<select id="<?php echo $e->id;?>" class="form-control" name="<?php echo $e->name;?>">
+			<?php if($e->placeholder):?>
+			<option style="display: none;" class="hint" value=""><?php echo $e->placeholder;?></option>
+			<?php endif;?>
 			<?php foreach($e->options as $key => $option):?>
 			<option value="<?php echo $key;?>"<?php if($e->value == $key):?> selected<?php endif;?>><?php echo $option;?></option>
 		  	<?php endforeach;?>
